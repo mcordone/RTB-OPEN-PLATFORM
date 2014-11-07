62fa911f959141a4c47b70bdb256e2091da80928
Miguel Cordones <miguel.cordones@teamaol.com>
2014-11-07 07:37:57 -0500

    Adding some modules

--- exchanges/requestProcessor.js
+++ exchanges/requestProcessor.js
@@ -1,6 +1,36 @@
 
-function processRequest(req, res){
-	console.log("******************* Request Processor \n" + req);
+exports.processRequest = function(req, res){
+	//TODO 
+	//1- get exchange name from req url
+	//2- get exchange request data
+	//3- call appropiate exchange translator
+	debugger;
 }
 
-module.exports = processRequest;
+function getExchangeNameFromURI(){
+	//TODO loop through a configuration and find the corresponding exchange object
+	var pathname = url.parse(request.url).pathname;
+
+}
+
+function getExchangeRequestBidData(request, response) {
+    var bidRequestData = "";
+
+	//qet request data
+	request.on('data', function(data) {
+	  bidRequestData += data;
+	  if(bidRequestData.length > 1e6) {
+	      bidRequestData = "";
+	      response.writeHead(204, {'Content-Type': 'text/json; charset=UTF-8'}).end();
+	      //TODO log HTTP Error 413 Request entity too large
+	      //response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+	      request.connection.destroy();
+	  }
+	});
+
+	request.on('end', function() {
+	  request.BID_DATA = JSON.parse(bidRequestData);
+	});
+}
+
+//module.exports = processRequest;
--- server.js
+++ server.js
@@ -1,64 +1,50 @@
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
-var reqProcessor = require('./exchanges/requestProcessor.js');
+var processRequest = require('./exchanges/requestProcessor.js');
 
 
 http.createServer(function(request, response) {
 
-    //new reqProcessor(request, response);
-    //getPostData(request, response);
-
     if(request.method == 'POST') {
-
-        getRequestBidData(request, response);
-
-    //response.sendHeader(200, { 'Content-Type': 'text/html' });
-    response.end('Done processing the request......');
-    } else {
-        response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
-        response.end();
+      processRequest(request, response);
+    }else {
+      // Send 405 Method Not Allowed, only POST accepted
+      response.writeHead(405, {'Content-Type': 'text/plain'});
+      response.end();
     }
+
+    response.end('Done processing the request......');
   
 }).listen(8124);
 
-function getRequestBidData(request, response) {
-    var queryData = "";
-    if(typeof callback !== 'function') return null;
+function requestHandler(request, response) {
+    var bidRequestData = "";
 
     if(request.method == 'POST') {
-        request.on('data', function(data) {
-            bidData += data;
-            if(bidData.length > 1e6) {
-                bidData = "";
-                response.writeHead(204, {'Content-Type': 'text/json; charset=UTF-8'}).end();
-                //TODO log HTTP Error 413 Request entity too large
-                //response.writeHead(413, {'Content-Type': 'text/plain'}).end();
-                request.connection.destroy();
-            }
-        });
 
-        request.on('end', function() {
-            request.BID_DATA = bidData;
-        });
+      //qet request data
+      request.on('data', function(data) {
+          bidRequestData += data;
+          if(bidRequestData.length > 1e6) {
+              bidRequestData = "";
+              response.writeHead(204, {'Content-Type': 'text/json; charset=UTF-8'}).end();
+              //TODO log HTTP Error 413 Request entity too large
+              //response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+              request.connection.destroy();
+          }
+      });
+
+      request.on('end', function() {
+          request.BID_DATA = JSON.parse(bidRequestData);
+      });
 
     } else {
-        response.writeHead(405, {'Content-Type': 'text/plain'});
-        response.end();
+      // Send 405 Method Not Allowed, only POST accepted
+      response.writeHead(405, {'Content-Type': 'text/plain'});
+      response.end();
     }
 }
-
-function getPostData (req, res) {
-        if (req.method == 'POST') {
-            var jsonString = '';
-            req.on('data', function (data) {
-                jsonString += data;
-            });
-            req.on('end', function () {
-                 console.log(JSON.parse(jsonString));
-            });
-        }
-    }
 
 
 function handle(response) {    
